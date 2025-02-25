6e3d00720e3c5768608f4e0d744e0865cbfa57fc
arawesome1 <105249379+arawesome1@users.noreply.github.com>
2025-02-25 11:26:25 +0530

    Update app.py
    removed Chroma

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
 from langchain.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
